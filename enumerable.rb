1115f9f98a3947fc7e74745e8b1a2e6dbbe16f8b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-16 12:45:22 +0100

    Add my_each enumerable

---
+++ enumerable.rb
@@ -0,0 +1,15 @@
+# rubocop:disable
+module Enumerable
+  # my_each_Enumberable method
+  def my_each
+    return to_enum unless block_given?
+
+    arr = to_a
+    i = 0
+    while i < arr.length
+      yield(arr[i])
+      i += 1
+    end
+    self
+  end
+end
