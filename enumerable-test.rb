436151f85664de8d5ab189fd7fdf1cfe592911b2
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-19 12:50:56 +0100

    Add my_each_with_index test

--- enumerable-test.rb
+++ enumerable-test.rb
@@ -3,3 +3,10 @@ puts '1.-------my_each-------'
 p(1..5).my_each { |num| puts "the number is #{num}" }
 p(%w[Sharon Leo Leila Brian Arun].my_each { |friend| puts "Hello, #{friend}" })
 puts ''
+
+# ..2...
+puts '2.-------my_each_with_index-------'
+p(%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts friend if index.even? })
+p(%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts "#{friend} is index #{index}" })
+p((1..5).my_each_with_index { |item, index| puts "#{item} is #{index}" })
+puts ''
