dd1847c29beb682d11453228597750fccf92745a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-22 11:00:41 +0100

    Add my_select rspec test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -21,14 +21,27 @@ describe Enumerable do
   describe '#my_each_with_index' do
     let(:range) { (1..5) }
     let(:arr) { %w[Sharon Leo Leila Brian Arun] }
+
     it 'should return the range of numbers' do
       result = []
       arr.my_each_with_index { |value, idx| result << value if idx.even? }
       expect(result).to eql(%w[Sharon Leila Arun])
     end
 
-    it 'should return the index of a range' do
+    it 'works with ranges' do
       expect(range.my_each_with_index { |_v, i| i }).to match_array([1, 2, 3, 4, 5])
+    end
+  end
+
+  describe 'my_select' do
+	let(:arr) { [1, 2, 3, 4, 5, 6] }
+	
+    it 'should return even numbers' do
+      expect(arr.my_select { |num| num.even? }).to eql([2, 4, 6])
+    end
+
+    it 'should return numbers greater than 3' do
+      expect(arr.my_select { |num| num > 3 }).to eql([4, 5, 6])
     end
   end
 end
