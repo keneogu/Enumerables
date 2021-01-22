f07321e581bbcbe340463bbc3e1530ad324ead7a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-22 10:49:29 +0100

    Add my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -17,4 +17,18 @@ describe Enumerable do
       expect(arr.my_each { |item| item }).not_to eql([1, 2, 3, 4])
     end
   end
+
+  describe '#my_each_with_index' do
+    let(:range) { (1..5) }
+    let(:arr) { %w[Sharon Leo Leila Brian Arun] }
+    it 'should return the range of numbers' do
+      result = []
+      arr.my_each_with_index { |value, idx| result << value if idx.even? }
+      expect(result).to eql(%w[Sharon Leila Arun])
+    end
+
+    it 'should return the index of a range' do
+      expect(range.my_each_with_index { |_v, i| i }).to match_array([1, 2, 3, 4, 5])
+    end
+  end
 end
