f4d86e68b1a5efc0de5d205e8dd81d826c143685
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-22 11:13:11 +0100

    Add my_all rspec test

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -34,14 +34,26 @@ describe Enumerable do
   end
 
   describe 'my_select' do
-	let(:arr) { [1, 2, 3, 4, 5, 6] }
-	
+    let(:arr) { [1, 2, 3, 4, 5, 6] }
+
     it 'should return even numbers' do
       expect(arr.my_select { |num| num.even? }).to eql([2, 4, 6])
     end
 
     it 'should return numbers greater than 3' do
       expect(arr.my_select { |num| num > 3 }).to eql([4, 5, 6])
+    end
+  end
+
+  describe 'my_all?' do
+    let(:arr) { [1, 2, 3, 4, 5, 6] }
+
+    it 'should return true if all the numbers are positive' do
+      expect(arr.my_all? { |num| num.positive? }).to be true
+    end
+
+    it 'checks if all the element in the array is an integer' do
+      expect([1, 2i, 'John'].my_all?(Numeric)).to be false
     end
   end
 end
